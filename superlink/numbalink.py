dded19cc30b287a09a62fddc4d09fefb76ecb577
Matt Bartos <mdbartos@umich.edu>
2020-01-27 22:40:28 -0500

    Speed up forward recurrence

--- superlink/numbalink.py
+++ superlink/numbalink.py
@@ -25,6 +25,65 @@ class NumbaLink(SuperLink):
                          bc_method, exit_hydraulics, end_length,
                          end_method)
 
+    def forward_recurrence(self):
+        """
+        Compute forward recurrence coefficients: T_ik, U_Ik, V_Ik, and W_Ik.
+        """
+        # Import instance variables
+        backward_I_I = self.backward_I_I  # Index of junction before junction Ik
+        forward_I_I = self.forward_I_I    # Index of junction after junction Ik
+        forward_I_i = self.forward_I_i    # Index of link after junction Ik
+        _I_end = self._I_end              # Junction at downstream end of superlink (y/n)
+        _I_1k = self._I_1k                # Index of first junction in each superlink
+        _I_2k = self._I_2k                # Index of second junction in each superlink
+        _i_1k = self._i_1k                # Index of first link in each superlink
+        _A_ik = self._A_ik                # Flow area in link ik
+        _E_Ik = self._E_Ik                # Continuity coefficient E_Ik
+        _D_Ik = self._D_Ik                # Continuity coefficient D_Ik
+        _a_ik = self._a_ik                # Momentum coefficient a_ik
+        _b_ik = self._b_ik                # Momentum coefficient b_ik
+        _c_ik = self._c_ik                # Momentum coefficient c_ik
+        _P_ik = self._P_ik                # Momentum coefficient P_ik
+        _T_ik = self._T_ik                # Recurrence coefficient T_ik
+        _U_Ik = self._U_Ik                # Recurrence coefficient U_Ik
+        _V_Ik = self._V_Ik                # Recurrence coefficient V_Ik
+        _W_Ik = self._W_Ik                # Recurrence coefficient W_Ik
+        _end_method = self._end_method    # Method for computing flow at pipe ends
+        NK = self.NK
+        nk = self.nk
+        # Compute coefficients for starting nodes
+        _E_2k = _E_Ik[_I_2k]
+        _D_2k = _D_Ik[_I_2k]
+        if _end_method == 'o':
+            _T_1k = self.T_1k(_a_ik[_i_1k], _b_ik[_i_1k], _c_ik[_i_1k])
+            _U_1k = self.U_1k(_E_2k, _c_ik[_i_1k], _A_ik[_i_1k], _T_1k)
+            _V_1k = self.V_1k(_P_ik[_i_1k], _D_2k, _c_ik[_i_1k], _T_1k)
+            _W_1k = self.W_1k(_A_ik[_i_1k], _T_1k)
+        else:
+            _T_1k = self.T_1k(_a_ik[_i_1k], _b_ik[_i_1k], _c_ik[_i_1k])
+            _U_1k = self.U_1k(_E_2k, _c_ik[_i_1k], _A_ik[_i_1k], _T_1k)
+            _V_1k = self.V_1k(_P_ik[_i_1k], _D_2k, _c_ik[_i_1k], _T_1k,
+                              _a_ik[_i_1k], _D_Ik[_I_1k])
+            _W_1k = self.W_1k(_A_ik[_i_1k], _T_1k, _a_ik[_i_1k], _E_Ik[_I_1k])
+        # I = 1, i = 1
+        _T_ik[_i_1k] = _T_1k
+        _U_Ik[_I_1k] = _U_1k
+        _V_Ik[_I_1k] = _V_1k
+        _W_Ik[_I_1k] = _W_1k
+        # I = 2, i = 2
+        numba_forward_recurrence(_T_ik, _U_Ik, _V_Ik, _W_Ik, _a_ik, _b_ik, _c_ik,
+                                 _P_ik, _A_ik, _E_Ik, _D_Ik, NK, nk, _I_2k, _i_1k)
+        # Try resetting
+        _T_ik[_i_1k] = _T_1k
+        _U_Ik[_I_1k] = _U_1k
+        _V_Ik[_I_1k] = _V_1k
+        _W_Ik[_I_1k] = _W_1k
+        # Export instance variables
+        self._T_ik = _T_ik
+        self._U_Ik = _U_Ik
+        self._V_Ik = _V_Ik
+        self._W_Ik = _W_Ik
+
     def solve_internals_ls_alt(self):
         NK = self.NK
         nk = self.nk
@@ -91,8 +150,89 @@ def ls_solve(_h_Ik, NK, nk, _k_1k, _i_1k, _I_1k, _U, _X, _b):
         _Ab = _Ak.T @ _bk
         # If want to prevent singular matrix, set ( diag == 0 ) = 1
         for i in range(nlinks - 1):
-            if (_AA[i, i] == 0.):
-                _AA[i, i] = 1.
+            if (_AA[i, i] == 0.0):
+                _AA[i, i] = 1.0
         _h_inner = np.linalg.solve(_AA, _Ab)
         _h_Ik[jstart+1:jstart+nlinks] = _h_inner
     return _h_Ik
+
+@njit
+def safe_divide(num, den):
+    if (den == 0):
+        return 0
+    else:
+        return num / den
+
+@njit
+def U_Ik(E_Ip1k, c_ik, A_ik, T_ik, g=9.81):
+    """
+    Compute forward recurrence coefficient 'U' for node I, superlink k.
+    """
+    num = E_Ip1k * c_ik - g * A_ik
+    den = T_ik
+    result = safe_divide(num, den)
+    return result
+
+@njit
+def V_Ik(P_ik, a_ik, D_Ik, D_Ip1k, c_ik, A_ik, E_Ik, V_Im1k, U_Im1k, T_ik, g=9.81):
+    """
+    Compute forward recurrence coefficient 'V' for node I, superlink k.
+    """
+    t_0 = P_ik
+    t_1 = a_ik * D_Ik
+    t_2 = D_Ip1k * c_ik
+    t_3 = (g * A_ik - E_Ik * a_ik)
+    t_4 = V_Im1k + D_Ik
+    t_5 = U_Im1k - E_Ik
+    t_6 = T_ik
+    # TODO: There is still a divide by zero here
+    num = (t_0 + t_1 - t_2 - (t_3 * t_4 / t_5))
+    den = t_6
+    result = safe_divide(num, den)
+    return  result
+
+@njit
+def W_Ik(A_ik, E_Ik, a_ik, W_Im1k, U_Im1k, T_ik, g=9.81):
+    """
+    Compute forward recurrence coefficient 'W' for node I, superlink k.
+    """
+    num = -(g * A_ik - E_Ik * a_ik) * W_Im1k
+    den = (U_Im1k - E_Ik) * T_ik
+    result = safe_divide(num, den)
+    return result
+
+@njit
+def T_ik(a_ik, b_ik, c_ik, A_ik, E_Ik, U_Im1k, g=9.81):
+    """
+    Compute forward recurrence coefficient 'T' for link i, superlink k.
+    """
+    t_0 = a_ik + b_ik + c_ik
+    t_1 = g * A_ik - E_Ik * a_ik
+    t_2 = U_Im1k - E_Ik
+    result = t_0 - safe_divide(t_1, t_2)
+    return result
+
+@njit
+def numba_forward_recurrence(_T_ik, _U_Ik, _V_Ik, _W_Ik, _a_ik, _b_ik, _c_ik,
+                             _P_ik, _A_ik, _E_Ik, _D_Ik, NK, nk, _I_2k, _i_1k):
+    for k in range(NK):
+        # Start at second junction
+        start_I = _I_2k[k]
+        start_i = _i_1k[k] + 1
+        nlinks = nk[k]
+        for i in range(nlinks - 1):
+            _i_next = start_i + i
+            _I_next = start_I + i
+            _Im1_next = _I_next - 1
+            _Ip1_next = _I_next + 1
+            _T_ik[_i_next] = T_ik(_a_ik[_i_next], _b_ik[_i_next], _c_ik[_i_next],
+                                  _A_ik[_i_next], _E_Ik[_I_next], _U_Ik[_Im1_next])
+            _U_Ik[_I_next] = U_Ik(_E_Ik[_Ip1_next], _c_ik[_i_next],
+                                  _A_ik[_i_next], _T_ik[_i_next])
+            _V_Ik[_I_next] = V_Ik(_P_ik[_i_next], _a_ik[_i_next], _D_Ik[_I_next],
+                                  _D_Ik[_Ip1_next], _c_ik[_i_next], _A_ik[_i_next],
+                                  _E_Ik[_I_next], _V_Ik[_Im1_next], _U_Ik[_Im1_next],
+                                  _T_ik[_i_next])
+            _W_Ik[_I_next] = W_Ik(_A_ik[_i_next], _E_Ik[_I_next], _a_ik[_i_next],
+                                  _W_Ik[_Im1_next], _U_Ik[_Im1_next], _T_ik[_i_next])
+    return 1
