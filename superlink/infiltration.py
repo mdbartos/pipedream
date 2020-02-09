9e10c6e762150fa18b5b159fffb4024edf0a8610
Matt Bartos <mdbartos@umich.edu>
2020-02-09 14:56:02 -0500

    Add vectorized infiltration

--- superlink/infiltration.py
+++ superlink/infiltration.py
@@ -9,8 +9,9 @@ class GreenAmpt():
         self.theta_r = theta_r
         self.psi_b = psi_b
         self.theta_d = (theta_s - theta_i)
-        self.theta_du = self.theta_d
-        self.theta_dmax = self.theta_d
+        self.theta_du = np.copy(self.theta_d)
+        self.theta_dmax = np.copy(self.theta_d)
+        self.N = len(Ks)
         # Initialize constant parameters
         m_to_in = 39.97
         in_to_m = 0.0254
@@ -19,11 +20,12 @@ class GreenAmpt():
         self.kr = np.sqrt(Ks * m_to_in) / 75
         self.Tr = 4.5 / np.sqrt(Ks * m_to_in)
         # Initialize time-dependent variables
-        self.d = 0.                  # Ponded depth
-        self.F = 0.                  # Cumulative infiltration (m)
-        self.f = 0.                  # Instantaneous infiltration rate (m/hr)
-        self.T = 0.                  # Recovery time before next event (hr)
-        self.is_saturated = False
+        self.d = np.zeros(self.N, dtype=float)              # Ponded depth
+        self.F = np.zeros(self.N, dtype=float)              # Cumulative infiltration (m)
+        self.f = np.zeros(self.N, dtype=float)              # Instantaneous infiltration rate (m/hr)
+        self.T = np.zeros(self.N, dtype=float)              # Recovery time before next event (hr)
+        self.is_saturated = np.zeros(self.N, dtype=bool)
+        self.iter_count = 0
 
     def s2(self, theta_i, theta_s, theta_r, Ks, psi_b, lambda_o):
         num = 2*(theta_s - theta_i) * Ks * psi_b * ((theta_s - theta_r)**(1 / lambda_o + 3)
@@ -46,90 +48,148 @@ class GreenAmpt():
         # 2. Decrease recovery time
         self.T -= dt
 
-    def unsaturated_case_1(self, dt):
+    def unsaturated_case_1(self, dt, case_1):
         # 3. If available rainfall is zero:
-        kr = self.kr
-        Lu = self.Lu
-        theta_dmax = self.theta_dmax
-        T = self.T
-        self.f = 0.
+        kr = self.kr[case_1]
+        Lu = self.Lu[case_1]
+        theta_dmax = self.theta_dmax[case_1]
+        T = self.T[case_1]
+        # Variables to be written
+        F = self.F[case_1]
+        f = self.f[case_1]
+        theta_du = self.theta_du[case_1]
+        theta_d = self.theta_d[case_1]
+        # Set infiltration rate to zero
+        f[:] = 0.
         # Recover upper zone moisture deficit and cumulative infiltration
         d_theta = kr * theta_dmax * dt
-        self.theta_du += d_theta
-        self.theta_du = np.minimum(np.maximum(self.theta_du, 0), theta_dmax)
-        self.F -= d_theta * Lu
+        theta_du += d_theta
+        theta_du[:] = np.minimum(np.maximum(theta_du, 0), theta_dmax)
+        F -= d_theta * Lu
         # If min recovery time has expired, mark beginning of new rainfall event
-        if (T <= 0):
-            self.theta_d = theta_du
-            self.F = 0.
+        cond = (T <= 0)
+        if cond.any():
+            theta_d[cond] = theta_du
+            F[cond] = 0.
+        # Export
+        self.F[case_1] = F
+        self.f[case_1] = f
+        self.theta_du[case_1] = theta_du
+        self.theta_d[case_1] = theta_d
 
-    def unsaturated_case_2(self, dt, ia):
+    def unsaturated_case_2(self, dt, ia, case_2):
         # 4. If available rainfall does not exceed saturated hydr. conductivity
-        Lu = self.Lu
-        theta_dmax = self.theta_dmax
-        self.f = ia
+        ia = ia[case_2]
+        Lu = self.Lu[case_2]
+        theta_dmax = self.theta_dmax[case_2]
+        # Variables to be written
+        F = self.F[case_2]
+        f = self.f[case_2]
+        theta_du = self.theta_du[case_2]
+        f[:] = ia
         dF = ia * dt
-        self.F += dF
-        self.theta_du -= dF / Lu
-        self.theta_du = np.minimum(np.maximum(self.theta_du, 0), theta_dmax)
+        F += dF
+        theta_du -= dF / Lu
+        theta_du[:] = np.minimum(np.maximum(theta_du, 0), theta_dmax)
+        # Export
+        self.F[case_2] = F
+        self.f[case_2] = f
+        self.theta_du[case_2] = theta_du
 
-    def unsaturated_case_3(self, dt, ia):
+    def unsaturated_case_3(self, dt, ia, case_3):
         # 5. If availble rainfall rate exceeds saturated hydr. conductivity
-        Ks = self.Ks
-        Lu = self.Lu
-        theta_dmax = self.theta_dmax
-        psi_f = self.psi_f
-        theta_d = self.theta_d
-        F = self.F
+        orig_ia = np.copy(ia)
+        ia = ia[case_3]
+        is_saturated = self.is_saturated[case_3]
+        Ks = self.Ks[case_3]
+        Lu = self.Lu[case_3]
+        theta_dmax = self.theta_dmax[case_3]
+        psi_f = self.psi_f[case_3]
+        theta_d = self.theta_d[case_3]
+        # Variables to write
+        F = self.F[case_3]
+        f = self.f[case_3]
+        theta_du = self.theta_du[case_3]
         # Reset recovery time
-        self.T = self.Tr
+        self.T[case_3] = self.Tr[case_3]
         # Compute volume needed to saturate surface layer
         Fs = Ks * psi_f * theta_d / (ia - Ks)
-        if (F >= Fs):
-            self.is_saturated = True
-            # Run saturated conditions
-            self.saturated_case(dt, ia)
-        elif (F + ia * dt < Fs):
-            # TODO: DRY
-            self.f = ia
-            dF = ia * dt
-            self.F += dF
-            self.theta_du -= dF / Lu
-            self.theta_du = np.minimum(np.maximum(self.theta_du, 0), theta_dmax)
-        else:
-            # Solve integrated equation
-            sub_dt = dt - (Fs - F) / ia
-            F_2 = scipy.optimize.newton(self.integrated_green_ampt, F,
+        cond_0 = (F >= Fs)
+        cond_1 = (F + ia * dt < Fs) & (~cond_0)
+        cond_2 = (~cond_0 & ~cond_1)
+        # For first case
+        if cond_0.any():
+            # Set to saturated
+            is_saturated[cond_0] = True
+            self.is_saturated[case_3] = is_saturated
+            # Run saturated case
+            sat_case = np.copy(case_3)
+            sat_case[~cond_0] = False
+            self.saturated_case(dt, orig_ia, sat_case)
+            F[cond_0] = self.F[case_3][cond_0]
+            f[cond_0] = self.f[case_3][cond_0]
+            theta_du[cond_0] = self.theta_du[case_3][cond_0]
+        # Run second case
+        if cond_1.any():
+            f[cond_1] = ia[cond_1]
+            dF = ia[cond_1] * dt
+            F[cond_1] += dF
+            theta_du[cond_1] -= dF[cond_1] / Lu[cond_1]
+            theta_du[cond_1] = np.minimum(np.maximum(theta_du[cond_1], 0),
+                                                theta_dmax[cond_1])
+        # Run third case
+        # Solve integrated equation
+        if cond_2.any():
+            sub_dt = dt - (Fs[cond_2] - F[cond_2]) / ia[cond_2]
+            F_2 = scipy.optimize.newton(self.integrated_green_ampt, F[cond_2],
                                         self.derivative_green_ampt,
-                                        args=(Fs, sub_dt, Ks, theta_d, psi_f))
-            dF = F_2 - F
-            self.F = F_2
-            self.theta_du -= dF / Lu
-            self.theta_du = np.minimum(np.maximum(self.theta_du, 0), theta_dmax)
-            self.f = dF / dt
+                                        args=(Fs[cond_2], sub_dt, Ks[cond_2],
+                                                theta_d[cond_2], psi_f[cond_2]))
+            dF = F_2 - F[cond_2]
+            F[cond_2] = F_2
+            theta_du[cond_2] -= dF / Lu[cond_2]
+            theta_du[cond_2] = np.minimum(np.maximum(theta_du[cond_2], 0),
+                                                theta_dmax[cond_2])
+            f[cond_2] = dF / dt
+        # Export
+        self.F[case_3] = F
+        self.f[case_3] = f
+        self.theta_du[case_3] = theta_du
+        # TODO: Problem may be that these are being double set for cond_0 ^^^
 
-    def saturated_case(self, dt, ia):
-        Lu = self.Lu
-        Ks = self.Ks
-        theta_dmax = self.theta_dmax
-        theta_d = self.theta_d
-        psi_f = self.psi_f
-        F = self.F
+    def saturated_case(self, dt, ia, sat_case):
+        ia = ia[sat_case]
+        Lu = self.Lu[sat_case]
+        Ks = self.Ks[sat_case]
+        theta_dmax = self.theta_dmax[sat_case]
+        theta_d = self.theta_d[sat_case]
+        psi_f = self.psi_f[sat_case]
+        # Variables to write
+        F = self.F[sat_case]
+        f = self.f[sat_case]
+        theta_du = self.theta_du[sat_case]
+        is_saturated = self.is_saturated[sat_case]
         # Reset recovery time
-        self.T = self.Tr
+        self.T[sat_case] = self.Tr[sat_case]
         # Solve integrated equation
         F_2 = scipy.optimize.newton(self.integrated_green_ampt, F,
                                     self.derivative_green_ampt,
                                     args=(F, dt, Ks, theta_d, psi_f))
         dF = F_2 - F
-        if (dF > ia * dt):
-            dF = ia * dt
+        cond = (dF > ia * dt)
+        if cond.any():
+            dF[cond] = ia[cond] * dt
             # Set current surface layer to unsaturated
-            self.is_saturated = False
-        self.F += dF
-        self.theta_du -= dF / Lu
-        self.theta_du = np.minimum(np.maximum(self.theta_du, 0), theta_dmax)
-        self.f = dF / dt
+            is_saturated[cond] = False
+        F += dF
+        theta_du -= dF / Lu
+        theta_du[:] = np.minimum(np.maximum(theta_du, 0), theta_dmax)
+        f[:] = dF / dt
+        # Export
+        self.F[sat_case] = F
+        self.f[sat_case] = f
+        self.theta_du[sat_case] = theta_du
+        self.is_saturated[sat_case] = is_saturated
 
     def integrated_green_ampt(self, F_2, F_1, dt, Ks, theta_d, psi_s):
         # NOTE: I'm putting abs to prevent log error
@@ -146,14 +206,18 @@ class GreenAmpt():
         Ks = self.Ks
         ia = self.available_rainfall(dt, i)
         self.decrement_recovery_time(dt)
-        if is_saturated:
-            self.saturated_case(dt, ia)
-        else:
-            if (ia == 0.):
-                self.unsaturated_case_1(dt)
-            elif (ia <= Ks):
-                self.unsaturated_case_2(dt, ia)
-            elif (ia > Ks):
-                self.unsaturated_case_3(dt, ia)
+        sat_case = is_saturated
+        unsat_case_1 = (~is_saturated & (ia == 0.))
+        unsat_case_2 = (~is_saturated & (ia <= Ks))
+        unsat_case_3 = (~is_saturated & (ia > Ks))
+        if sat_case.any():
+            self.saturated_case(dt, ia, sat_case)
+        if unsat_case_1.any():
+            self.unsaturated_case_1(dt, unsat_case_1)
+        if unsat_case_2.any():
+            self.unsaturated_case_2(dt, ia, unsat_case_2)
+        if unsat_case_3.any():
+            self.unsaturated_case_3(dt, ia, unsat_case_3)
+        self.iter_count += 1
 
 
