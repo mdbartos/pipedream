97c7f06efadd850a0c993751757b0e38cc80d84b
Matt Bartos <mdbartos@umich.edu>
2020-02-13 00:11:20 -0500

    Minor change to kalman filter

--- superlink/utils.py
+++ superlink/utils.py
@@ -58,10 +58,10 @@ def _kalman_semi_implicit(Z_next, P_x_k_k, A_1, A_2, b, H, C,
                           Qcov, Rcov):
     I = np.eye(A_1.shape[0])
     y_k1_k = b
-    A_1_inv = scipy.linalg.inv(A_1)
+    A_1_inv = np.linalg.inv(A_1)
     H_1 = H @ A_1_inv
     P_y_k1_k = A_2 @ P_x_k_k @ A_2.T + C @ Qcov @ C.T
-    L_y_k1 = P_y_k1_k @ H_1.T @ scipy.linalg.inv((H_1 @ P_y_k1_k @ H_1.T) + Rcov)
+    L_y_k1 = P_y_k1_k @ H_1.T @ np.linalg.inv((H_1 @ P_y_k1_k @ H_1.T) + Rcov)
     P_y_k1_k1 = (I - L_y_k1 @ H_1) @ P_y_k1_k
     b_hat = y_k1_k + L_y_k1 @ (Z_next - H_1 @ y_k1_k)
     P_x_k1_k1 = A_1_inv @ P_y_k1_k1 @ A_1_inv.T
