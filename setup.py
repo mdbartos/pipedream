da10fba27b53f1074122efbab3554d169ae279a8
Matt Bartos <mdbartos@umich.edu>
2020-10-22 14:06:13 -0500

    Update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pipedream-solver',
-      version='0.1',
+      version='0.1.1',
       description='Interactive hydrodynamic solver for sewer/stormwater networks',
       author='Matt Bartos',
       author_email='mdbartos@umich.edu',
