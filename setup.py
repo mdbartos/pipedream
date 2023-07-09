0fc0159c4d21ec531d55b6cbedaa55ddc2e85ff5
Matt Bartos <mdbartos@utexas.edu>
2023-07-09 12:25:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@
 from setuptools import setup
 
 setup(name='pipedream-solver',
-      version='0.2.1',
-      description='Interactive hydrodynamic solver for sewer/stormwater networks',
+      version='0.2.2',
+      description='🚰 Interactive hydrodynamic solver for pipe and channel networks',
+      long_description="🚰 Interactive hydrodynamic solver for pipe and channel networks",
+      long_description_content_type="text/x-rst",
       author='Matt Bartos',
-      author_email='mdbartos@umich.edu',
+      author_email='mdbartos@utexas.edu',
       url='https://mdbartos.github.io/pipedream',
       packages=["pipedream_solver"],
       install_requires=[
